96b8d3ceca6b71d1a062204348327408fa14e8b6
jerakrs <jerakrs@gmail.com>
2024-06-19 22:23:17 +0800

    2024.6.29

---
+++ codes/Leetcode/leetcode1302.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+
+        int sum = 0;
+        std::queue<TreeNode*> que;
+        que.push(root);
+        while (!que.empty()) {
+            int n = que.size();
+            sum = 0;
+            while (n--) {
+                TreeNode* node = que.front();
+                que.pop();
+                sum += node->val;
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+            }
+        }
+        return sum;
+    }
+};
